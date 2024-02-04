bdc46d08766072992e792817c08358bca8b01854
Рафаэль Гантус <rafael.gantus@bk.ru>
2024-02-04 12:10:21 +0300

    revue 14

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -133,7 +133,7 @@ function App() {
   }, [loggedIn, navigate]);
 
   function handleCardLike(card) {
-    const isLiked = card.likes.some(i => i._id === currentUser._id);
+    const isLiked = card.likes.some(i => i === currentUser._id);
     api.changeLikeStatus(card._id, !isLiked)
       .then((res) => {
         setCards((state) => state.map((c) => c._id === card._id ? res : c));
